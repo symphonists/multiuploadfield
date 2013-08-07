9d16b9570a2e4ac434793880c216f2c8c96412df
Nils Hörrmann <post@nilshoerrmann.de>
2013-08-07 14:59:18 +0200

    Whitespace

--- assets/multiupload.publish.js
+++ assets/multiupload.publish.js
@@ -7,30 +7,29 @@
 		multiupload.symphonyDuplicator({
 			orderable: false,
 			collapsible: false
-  		});
+		});
 
-  		// Select file
-  		multiupload.on('constructshow.duplicator', 'li', function(event) {
-   			var file = $(this).find('input[type="file"]').focus().trigger('click.multiupload');
+		// Select file
+		multiupload.on('constructshow.duplicator', 'li', function(event) {
+			var file = $(this).find('input[type="file"]').focus().trigger('click.multiupload');
 
- 			setTimeout(function() {
+			setTimeout(function() {
 				$('body').one('mousemove.multiupload', function() {
-					console.log('focus');
 					$(file).trigger('change.multiupload');
 				});
-  			}, 500);
-  		});
-  		multiupload.on('change.multiupload', 'input[type="file"]', function(event) {
-  			var name = this.value,
-  				instance = $(this).parents('.instance');
+			}, 500);
+		});
+		multiupload.on('change.multiupload', 'input[type="file"]', function(event) {
+			var name = this.value,
+				instance = $(this).parents('.instance');
 
-  			if(name != '') {
-  				instance.find('span').text(name.replace(/^.*[\\\/]/, '')).addClass('file');
-  			}
-  			else {
-  				instance.find('.destructor').trigger('click.duplicator');
-  			}
-  		});
+			if(name != '') {
+				instance.find('span').text(name.replace(/^.*[\\\/]/, '')).addClass('file');
+			}
+			else {
+				instance.find('.destructor').trigger('click.duplicator');
+			}
+		});
 	});
 
 })(window.jQuery);
