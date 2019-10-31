939cda4435bbd6ffdfae10295bdf1b27a55112cb
Juraj Kapsz <dev@jurajkapsz.sk>
2019-10-31 07:28:09 +0100

    SVG images meta autoupdate & version bump

--- extension.driver.php
+++ extension.driver.php
@@ -17,6 +17,39 @@ class extension_multiuploadfield extends Extension {
 
     public function update($previousVersion = null)
     {
+
+        // Prior version 1.6.3:
+        // Auto-update meta data of existing SVG entries
+        if(version_compare($previousVersion, '1.6.3', '<')) {
+
+            // Get multiupload fields
+            $multiupload_fields = FieldManager::fetch(null, null, 'ASC', 'id', 'multiupload');
+
+            foreach ($multiupload_fields as $field) {
+                
+                $table_id = $field->get('id');
+                $file_path = DOCROOT . $field->get('destination');
+                
+                // Get only SVG DB entries
+                $svg_files = Symphony::Database()->fetch("SELECT `id`, `file`, `mimetype` FROM `tbl_entries_data_$table_id` WHERE `mimetype` LIKE '%image/svg%'");
+                
+                foreach ($svg_files as $svg) {
+
+                    $entry_id = intval($svg['id']);
+                    $file = $file_path . '/' . $svg['file'];
+                    $mimetype = $svg['mimetype'];
+
+                    $meta = serialize(FieldMultiUpload::getMetaInfo($file, $mimetype));
+
+                    // Update SVG DB entry
+                    Symphony::Database()->query("UPDATE `tbl_entries_data_$table_id` SET `meta` = '" . $meta . "' WHERE `id` = $entry_id");
+
+                }
+
+            }
+
+        }
+            
     }
 
     public function uninstall()
